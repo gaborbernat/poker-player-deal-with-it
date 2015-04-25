a41c035a70f6d9f704a77d407f72fd2607fb786a
Gabor Bernat <gabor.bernat@gravityrd.com>
2015-04-25 10:08:04 +0200

    fix

--- player.py
+++ player.py
@@ -5,9 +5,10 @@ class Player(object):
         self.game_state = game_state
 
     def bet_request(self):
-        bet = int(2.5*self.game_state['minimum_raise'])
+        bet = int(int(self.game_state['current_buy_in']) + int(self.game_state['minimum_raise']))
         print('we bet {} for {}'.format(bet, self.game_state))
         return bet
 
     def showdown(self):
-        pass
+        print(self.game_state)
+
