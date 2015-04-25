301e209138000f796891c24ba8dbfbad11b772f3
Salaander <nagy.sandor.tibor@gravityrd.com>
2015-04-25 10:51:58 +0200

    Added should_we_fold and is_pair_in_hand

--- player.py
+++ player.py
@@ -17,25 +17,40 @@ class Player(object):
         self.game_state = game_state
 
     def bet_request(self):
-        mb = r(self.game_state, ['minimum_raise'], 0)
-        cb = r(self.game_state, ['current_buy_in'], 0)
-        bet = mb + cb
-        print('we bet {} for {}'.format(bet, self.game_state))
+
         # get our cards
-        us = self.getCards(self.getOurPlayer())
+        ourHand = self.get_cards(self.get_our_player())
+
+        # should we fold
+        if self.should_we_fold(ourHand):
+            bet = 0
+        else:
+            mb = r(self.game_state, ['minimum_raise'], 0)
+            cb = r(self.game_state, ['current_buy_in'], 0)
+            bet = mb + cb
+            print('we bet {} for {}'.format(bet, self.game_state))
+
         return bet
 
 
     # Return OUR player
-    def getOurPlayer(self):
+    def get_our_player(self):
         for player in self.game_state['players']:
             if player["name"] == self.team_name:
                 return player
 
 
     # Returns cards for a player json object
-    def getCards(self, player):
+    def get_cards(self, player):
         return player['hole_cards']
+
+
+    def is_pair_in_hand(self, hand):
+        return hand[0]["suit"] == hand[1]["suit"] and hand[0]["rank"] == hand[1]["rank"]
+
+    def should_we_fold(self, hand):
+        return hand[0]["suit"] != hand[1]["suit"] and hand[0]["rank"] != hand[1]["rank"] and hand[0]["rank"] not in ["J", "K", "Q", "A"] and hand[1]["rank"] not in ["J", "K", "Q", "A"]
+
 
     def showdown(self):
         print('showdown {}'.format(self.game_state))
